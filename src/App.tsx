a6e649de7b14b304209f4eedd59844843ee63af7
6210110713 <buk_327@hotmail.co.th>
2023-05-14 11:25:57 +0700

    Test React - edit- successful

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 import IndexPage from "./indexpage";
 import Layoutpage from "./component/layout/layoutpage";
@@ -15,6 +14,10 @@ function App() {
         <Route path="/" element={<IndexPage />}>
         </Route>
         <Route path="/:lang" element={<IndexPage />}>
+        </Route>
+        <Route path="layout/:lang" element={<Layoutpage/>}>
+        </Route>
+        <Route path="formandtable/:lang" element={<AllPageinForm />}>
         </Route>
         <Route path="/:lang/layout/:lang" element={<Layoutpage/>}>
         </Route>
